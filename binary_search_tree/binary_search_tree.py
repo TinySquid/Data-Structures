d93991242e8c9eaa02574ac4fb007a91424fd284
Michael Nunes <tinysquid.git@gmail.com>
2020-03-25 15:21:05 -0700

    MVP for day 1 of binary_search_tree.py

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('../queue_and_stack')
+
+sys.path.append("../queue_and_stack")
 from dll_queue import Queue
 from dll_stack import Stack
 
@@ -12,21 +13,70 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        # Create a node
+        node = BinarySearchTree(value)
+
+        # Check value vs self to determine which direction to go
+        if node.value >= self.value:
+            if self.right != None:
+                # Travel right
+                self.right.insert(value)
+            else:
+                self.right = node
+        else:
+            if self.left != None:
+                # Travel left
+                self.left.insert(value)
+            else:
+                self.left = node
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # Base case
+        if self.value == target:
+            return True
+        elif self.value > target:
+            if self.left != None:
+                return self.left.contains(target)
+        elif self.value < target:
+            if self.right != None:
+                return self.right.contains(target)
+
+        return False
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # Self ref
+        current_node = self
+
+        # Max value tracker
+        max_value = float("-inf")
+
+        # While we can traverse right
+        while current_node:
+            # Compare current node value with current max value
+            if current_node.value > max_value:
+                max_value = current_node.value
+            # Check if we can go right
+            if current_node.right != None:
+                # Set current_node pointer to right node for next iteration
+                current_node = current_node.right
+            else:
+                # Break out of loop
+                current_node = None
+
+        return max_value
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
 
     # DAY 2 Project -----------------------
 
